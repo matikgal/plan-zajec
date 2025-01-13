5549d28e03b6869a0f117e759f08a4c0115ecf5e
GaloszJakub <kubagalosz@gmail.com>
2025-01-13 18:57:26 +0100

    Push

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function NavbarComponent() {
+	return (
+		<div>
+			<div className="bg-[#2c2c2c]">
+				<p className="text-2xl p-4 text-[#f2f2f2]">To będzie Plan</p>
+			</div>
+		</div>
+	)
+}
